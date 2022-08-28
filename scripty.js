e623491d0e5021888c9f361a69a8ecd1071b3b09
Malek <malekbusari@gmail.com>
2022-08-28 14:33:14 +0100

    Added event listener for slider

--- scripty.js
+++ scripty.js
@@ -10,23 +10,32 @@
 let grid = parseInt(document.querySelector('#slider').value);
 console.log(grid);
 
+let color = 'black'
+
 const container = document.querySelector('.container');
 const div = document.createElement('div');
-grid = 10;
 
-function createGrid(grid) {
-    for (let i = 0; i < grid; i++) {
+
+function createGrid(number = 16) {
+    // clear grid
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+
+    // grid creation
+    for (let i = 0; i < number; i++) {
         // create rows
         const rows = document.createElement('div');
         rows.classList.toggle('row');
         container.appendChild(rows);
         // create coloumns
-        for (let j = 0; j < grid; j++) {
+        for (let j = 0; j < number; j++) {
             const box = document.createElement('div');
             box.classList.toggle('box');
             rows.appendChild(box);
         };
     };
+    
 };
 
 function gridColor(boxSelection, color) {
@@ -34,19 +43,24 @@ function gridColor(boxSelection, color) {
 }
 
 
-
-createGrid(grid)
-
 function main() {
-    // listen for mouse-over box
+
+     // change grid size
+     const slider = document.querySelector('#slider');
+     slider.addEventListener('mouseup', () => {
+        let sliderValue = document.querySelector('#slider').value;
+        createGrid(sliderValue)
+     })
+
+    // change box color 
     const boxSelection = document.querySelectorAll('.box')
     boxSelection.forEach((selectedBox) => {
         selectedBox.addEventListener('mouseover', () => {
             selectedBox.style.backgroundColor = color;
         });
     });
+
 }
 
-let color = 'black'
-
+createGrid()
 main()
